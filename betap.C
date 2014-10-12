d785e98e6a34e3bba477927140944fe0dad62ffe
Ross Johnson <ross@homemail.org>
2014-10-12 13:36:07 -0500

    added comment that FindPairings() is the primary interface

--- betap.C
+++ betap.C
@@ -13,6 +13,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 */
 
 /* this C++ code finds the best pairings for one section and one round according to USCF rules
+ * the primary interface is FindPairings() with the PlayerVector input
  * the greedy algorithm considers all possible swaps of any two players and keeps the best single swap (if better) for each iteration
  * multiple swaps per iteration are considered after no improvements can be made with a single swap, but only if multiple swaps are requested
  * the initial position is one possible pairing, a hint that might yield a better local minimum in this global minimization problem
